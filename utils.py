56ab437bea878e8c40af1684f9a8547bcb980388
Curtis Maloney <curtis@tinbrain.net>
2016-08-16 10:11:09 +1000

    Add utils module

---
+++ utils.py
@@ -0,0 +1,33 @@
+
+class Int(object):
+    '''
+    Hepler to provide literal ints in templates
+    '''
+    def __getitem__(self, key):
+        return int(key)
+
+
+
+class _float(float):
+    def __getitem__(self, key):
+        return self + (float(key) / 10 ** len(key))
+
+
+class Float(object):
+    '''
+    Helper to provide literal floats in templates
+    '''
+    def __getitem__(self, key):
+        return _float(key)
+
+
+class String(object):
+    def __getitem__(self, key):
+        return key
+
+
+literal = {
+    'int': Int(),
+    'float': Float(),
+    'str': String(),
+}

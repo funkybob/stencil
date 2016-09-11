9b086862cb6fa353c373d046941324cbbe16cc54
Curtis Maloney <curtis@tinbrain.net>
2016-09-11 20:51:43 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='stencil-template',
+    version='1.0.1',
+    description='A template engine light enough to embed in your project.',
+    url='https://github.com/funkybob/stencil/',
+    author='Curtis Maloney',
+    license='CC BY-SA 3.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='template',
+    py_modules=['stencil',],
+)

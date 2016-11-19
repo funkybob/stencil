671a745177c97b8b2d72b527fdbae27af7ac1851
Pedro Salgado <steenzout@ymail.com>
2016-11-18 19:17:24 -0700

    changes to make test run on Python3.

--- tests/integration/test_output.py
+++ tests/integration/test_output.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
+
 import os
 import glob
 import json
+import string
 
 import stencil
 
@@ -10,21 +14,21 @@ import stencil
 def test_output():
 
     stencil.FILTERS.update({
-        'title': unicode.title,
+        'title': string.capwords,
     })
 
     loader = stencil.TemplateLoader(['tmpl/'])
 
     for fn in sorted(glob.glob('tests/integration/tmpl/*.tpl')):
-        print fn,
+        print(fn,)
         base, ext = os.path.splitext(fn)
 
         try:
             t = loader.load(os.path.basename(fn))
-        except StandardError:
+        except Exception:
             import traceback
             traceback.print_exc()
-            print "FAIL"
+            print('FAIL')
             continue
 
         with open(base + '.out', 'r') as fin:
@@ -37,4 +41,4 @@ def test_output():
         result = t.render(c)
 
         assert result == out, 'Mismatched output for %r\n%r\n%r' % (fn, out, result)
-        print "OK"
+        print('OK')

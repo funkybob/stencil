95ab0ee0acc8676ae53cf4cbf5b61820be5c8f67
Pedro Salgado <steenzout@ymail.com>
2016-11-21 09:29:07 -0700

    added test for stencil.tokenise() function.

--- tests/unit/test_lib.py
+++ tests/unit/test_lib.py
@@ -1,4 +1,9 @@
-from stencil import BlockNode
+# -*- coding: utf-8 -*-
+
+
+import stencil
+
+from stencil import BlockNode, Token
 
 
 class TestTag(BlockNode):
@@ -6,3 +11,27 @@ class TestTag(BlockNode):
 
     def render(self, context, output):
         output.write(u'test')
+
+
+def test_tokenise():
+    """Test :py:func:`stencil.tokenise` function."""
+
+    it_token = stencil.tokenise('abc {{ x }} xyz')
+
+    token = it_token.next()
+    assert token is not None
+    assert isinstance(token, Token)
+    assert token.type == 'text'
+    assert token.content == 'abc '
+
+    token = it_token.next()
+    assert token is not None
+    assert isinstance(token, Token)
+    assert token.type == 'var'
+    assert token.content == 'x'
+
+    token = it_token.next()
+    assert token is not None
+    assert isinstance(token, Token)
+    assert token.type == 'text'
+    assert token.content == ' xyz'

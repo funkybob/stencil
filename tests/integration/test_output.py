a901efb8aa35d1577b0f2588c94736ca19aa06d5
Pedro Salgado <steenzout@ymail.com>
2016-11-22 09:57:34 -0700

    split tests. added auxiliary functions.

--- tests/integration/test_output.py
+++ tests/integration/test_output.py
@@ -17,38 +17,87 @@ import stencil
 
 class IntegrationTestCase(unittest.TestCase):
 
-    @staticmethod
-    def test_output():
+    dir_tpl = 'tests/integration/tmpl/'
 
+    @staticmethod
+    def basename(file_name, extension):
+        """Return full path."""
+        return '%s/%s.%s' % (IntegrationTestCase.dir_tpl, file_name, extension)
+
+    def setUp(self):
         stencil.FILTERS.update({
             'title': string.capwords,
         })
 
-        loader = stencil.TemplateLoader(['tests/integration/tmpl/'])
+        self.loader = stencil.TemplateLoader([IntegrationTestCase.dir_tpl])
 
-        counter = 0
-        for fn in sorted(glob.glob('tests/integration/tmpl/*.tpl')):
-            print(fn,)
-            base, ext = os.path.splitext(fn)
+    def assert_output(self, base):
+        try:
+            t = self.loader.load('%s.tpl' % base)
+        except:
+            exc_args = sys.exc_info()
+            assert False, '[%s]\n%s' % (
+                base, ''.join(traceback.format_exception(*exc_args))
+            )
 
-            try:
-                print(fn,)
-                print(os.path.basename(fn))
-                t = loader.load(os.path.basename(fn))
-            except:
-                eargs = sys.exc_info()
-                assert False, '[%s]\n%s' % (fn, ''.join(traceback.format_exception(*eargs)))
+        out = self.read_text_data(base)
+        data = self.read_json_data(base)
 
-            with open(base + '.out', 'r') as fin:
-                out = fin.read()
+        c = stencil.Context(data)
+        result = t.render(c)
 
-            with open(base + '.json', 'r') as fin:
-                data = json.load(fin)
+        assert result == out, 'Mismatched output for %r\n%r\n%r' % (fn, out, result)
 
-            c = stencil.Context(data)
-            result = t.render(c)
+    def read_json_data(self, file_name):
+        """Return JSON data for the given template test.
 
-            assert result == out, 'Mismatched output for %r\n%r\n%r' % (fn, out, result)
-            counter += 1
+        Args:
+            file_name (str): the template being tested.
 
-        assert counter == 10, 'Expected 10 test runs as opposed to %d' % counter
+        Returns:
+            (str): JSON.
+        """
+        with open(self.basename(file_name, 'json'), 'r') as fin:
+            return json.load(fin)
+
+    def read_text_data(self, file_name):
+        """Return expected output for the given template test.
+
+        Args:
+            file_name (str): the template being tested.
+
+        Returns:
+            (str): output.
+        """
+        with open(self.basename(file_name, 'out'), 'r') as fin:
+            return fin.read()
+
+    def test_simple(self):
+        self.assert_output('01_simple')
+
+    def test_for(self):
+        self.assert_output('02_for')
+
+    def test_if(self):
+        self.assert_output('03_if')
+
+    def test_ifnot(self):
+        self.assert_output('04_ifnot')
+
+    def test_filter(self):
+        self.assert_output('05_filter')
+
+    def test_include(self):
+        self.assert_output('06_include')
+
+    def test_var(self):
+        self.assert_output('07_var')
+
+    def test_comment(self):
+        self.assert_output('08_comment')
+
+    def test_load(self):
+        self.assert_output('09_load')
+
+    def test_extends(self):
+        self.assert_output('10_extends')

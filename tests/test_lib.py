f98b36a7d9e2186474830434eab8e3db025511bb
Pedro Salgado <steenzout@ymail.com>
2016-11-22 09:20:35 -0700

    moved this content to its own module to test_output can use it for one of the test cases.

---
+++ tests/test_lib.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Module used in test_output module."""
+
+from stencil import BlockNode
+
+
+class TestTag(BlockNode):
+    name = 'test'
+
+    def render(self, context, output):
+        output.write(u'test')

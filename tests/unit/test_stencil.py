1eea70f8f378477b216b608aaa93e524a900cdf8
Curtis Maloney <curtis@tinbrain.net>
2017-07-26 21:17:43 +1000

    Add simple context push test

--- tests/unit/test_stencil.py
+++ tests/unit/test_stencil.py
@@ -30,3 +30,14 @@ class ModuleTestCase(unittest.TestCase):
         assert isinstance(token, Token)
         assert token.type == 'text'
         assert token.content == ' xyz'
+
+
+class ContextTestCase(unittest.TestCase):
+
+    def test_push(self):
+        ctx = stencil.Context({'a': 1})
+        self.assertEqual(ctx['a'], 1)
+        self.assertIsNone(ctx['None'])
+        with ctx.push(a=2):
+            self.assertEqual(ctx['a'], 2)
+        self.assertEqual(ctx['a'], 1)

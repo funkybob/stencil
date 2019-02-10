94ca9eaa3fd9a4e21101c2c95489d64de2c74fd8
Curtis Maloney <curtis@tinbrain.net>
2019-02-10 12:29:11 +1100

    Bumped version to 3.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def test_suite():
 
 setup(
     name='stencil-template',
-    version='3.0.2',
+    version='3.0.3',
     description='A template engine light enough to embed in your project.',
     url='https://github.com/funkybob/stencil/',
     author='Curtis Maloney',
